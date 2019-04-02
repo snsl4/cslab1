3f392257f4d26bafa0003a2732b9f56360a2f3d5
snsl <lebedev.snsl99@gmail.com>
2019-04-02 13:20:50 +0300

    lab2

---
+++ lab2_1c.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Первое число");
+            string num1 = Console.ReadLine();
+            Console.WriteLine("Второе число");
+            string num2 = Console.ReadLine();
+            multiplication(num1, num2);
+            Console.ReadKey();
+        }
+
+        public static void multiplication(string num1, string num2)
+        {
+            Int64 multiplicand, multiplier, product = 0, startMultiplicand, startMultiplier;
+            startMultiplicand = multiplicand = Int32.Parse(num1);
+            startMultiplier = multiplier = Int32.Parse(num2);
+            for (int i = 0; i < 32; ++i)
+            {
+                Console.WriteLine("Шаг " + (i + 1) + ":\n");
+
+                Console.WriteLine("Умножаемое:\n" + FinishStringWithZeros(Convert.ToString(multiplicand, 2)) +
+                    "\nУмножитель:\n" + FinishStringWithZeros(Convert.ToString(multiplier, 2)) + "\n");
+
+                short lsb = (short)(multiplier & 1);
+
+                if (lsb == 1)
+                {
+                    Console.WriteLine("Добавляем умножаемое и продукт\n");
+                    product += multiplicand;
+                }
+                Console.WriteLine("Продукт:\n" + FinishStringWithZeros(Convert.ToString(product, 2)) + "\n");
+                Console.WriteLine("Сдвиг умножаемого влево");
+                Console.WriteLine("Сдвиг умножителя вправо");
+
+                multiplicand <<= 1;
+                multiplier >>= 1;
+            }
+
+            Console.WriteLine("\n" + Convert.ToString(startMultiplicand, 2) +
+                "*" +
+                Convert.ToString(startMultiplier, 2) +
+                "=" +
+                Convert.ToString(product, 2) + "\n");
+
+            Console.WriteLine(startMultiplicand + " * " + startMultiplier + " = " + product);
+        }
+
+        static string FinishStringWithZeros(string val)
+        {
+            int count = 32 - val.Length;
+            string head = "";
+            for (int i = 0; i < count; ++i)
+                head += "0";
+            return head + val;
+        }
+    }
+}
---
+++ lab2_2a.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DivMod(0b110010, 0b1010);
+
+            Console.ReadKey();
+        }
+
+        public static void DivMod(Int32 dividend, Int32 divisor)
+        {
+            int d = dividend;
+            int r = divisor;
+
+            while (r < dividend)
+            {
+                r <<= 1;
+            }
+            Console.WriteLine(dividend + " / " + divisor);
+            Console.WriteLine(Convert.ToString(Convert.ToInt32(dividend), 2) + " / " + Convert.ToString(Convert.ToInt32(divisor), 2) + "\n");
+            Console.WriteLine("Делимое  " + Convert.ToString(Convert.ToInt32(d), 2));
+            Console.WriteLine("Делитель   " + Convert.ToString(Convert.ToInt32(r), 2) + "\n");
+            string result = string.Empty;
+            for (int i = 0; i < 128; i++)
+            {
+                Console.WriteLine("Шаг " + (i+1));
+                if (d < divisor && r < divisor)
+                {
+                    break;
+                }
+                if (r > d)
+                {
+                    r >>= 1;
+                    Console.WriteLine("Сдвиг");
+                    result += "0";
+                }
+                else
+                {
+                    d -= r;
+                    Console.WriteLine("Делимое - делитель");
+                    result += "1";
+                    r >>= 1;
+                }
+                Console.WriteLine("Делимое  " + Convert.ToString(Convert.ToInt32(d), 2));
+                Console.WriteLine("Делитель   " + Convert.ToString(Convert.ToInt32(r), 2));
+                Console.WriteLine("Результат = " + result + "\n");
+
+            }
+            Console.WriteLine("Результат : " + Convert.ToInt32(result, 2) + " ( " + result + " ) " + "   остаток : " + d + " ( " + Convert.ToString(Convert.ToInt32(d), 2) + " ) ");
+
+        }
+    }
+}
